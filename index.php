6d8b09095e57a349f11590396994ecf810797ae4
Winex <36075745+WinexYT@users.noreply.github.com>
2021-03-07 03:02:55 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <link rel="stylesheet" href="reset.css">
+  <link rel="shortcut icon" href="images/download.ico" type="image/x-icon">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+  <title>Kawaii.club</title>
+  <meta charset="utf-8">
+</head>
+
+<body>
+
+  <div class="block1">
+
+    <div class="idk">
+      <div class="orangeline2">
+
+      </div>
+      <div class="navbar">
+        <h2>#kawaii.</h2>
+        <li><a href="#home">Menu</a></li>
+        <li><a href="#blogs">Blogs</a></li>
+        <li><a href="#about">About us</a></li>
+        <li><a href="#contacts">Contacts</a></li>
+      </div>
+    </div>
+    <div class="orangeline">
+
+      <h2>GRAND</h2>
+      <p>OPENING</p>
+
+      <button>READ MORE</button>
+    </div>
+    <img src="images/block1.png" alt="">
+  </div>
+  </div>
+  <div class="block2">
+
+
+    <div class="class">
+      <p class="fig">
+        <img src="images/block2.png" alt="">
+      </p>
+      <div class="orangeline3">
+        <div class="randomtext">
+          <li>| Sed do eiusmod tempor incididunt ut Labore et</li>
+          <li>| Dolore magna aliqua</li>
+          <li>| Quis ipsum suspendisse ultrices gravida</li>
+          <div class="vertical_text">
+            <p>new format</p>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+
+
+  </div>
+  <div class="block3">
+
+
+    <div class="test3">
+
+
+      <div class="column1">
+        <h2>| new format</h2>
+        <p>Lorem ipsum dolor sit amet, consecte
+          sectetur adipisicing elit, tation omne
+          ullamco laboris nisi ut aliqolore.
+        </p>
+      </div>
+      <div class="column2">
+        <h2>|| master classes</h2>
+        <p>Lorem ipsum dolor sit amet, consecte
+          sectetur adipisicing elit, tation omne
+          ullamco laboris nisi ut aliqolore.
+        </p>
+      </div>
+      <div class="column3">
+        <h2>||| pizza restaurant</h2>
+        <p>Lorem ipsum dolor sit amet, consecte
+          sectetur adipisicing elit, tation omne
+          ullamco laboris nisi ut aliqolore.
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="block4">
+
+    <div class="rectangle3">
+      <div class="column4">
+        <h3>| EIUSMOD SED DO</h3>
+        <p>(tempor, incididunt, utas, labore, etws)</p>
+        <h4>10$<h4>
+      </div>
+      <div class="column5">
+        <h3>|| DO DOLORE</h3>
+        <p>(magna, aliqua, lorem) </p>
+        <h4>15$<h4>
+
+      </div>
+      <div class="column6">
+        <h3>||| EIUSMOD SED DO</h3>
+        <p>(tempor, incididunt, utas, labore, etws)</p>
+        <p>(tempor, incididunt, utas, labore, etws)</p>
+        <h4>10$<h4>
+
+      </div>
+    </div>
+    <div class="rectangle2">
+      <div class="column7">
+        <h3>||| _ top in menu</h3>
+        <h4>|| _ sale iteam</h4>
+        <h5>| _ made by shef</h5>
+      </div>
+    </div>
+    <div class="rectangle1">
+      <p>OUR NEW MENU</p>
+    </div>
+
+
+    <h2>#kawaii.</h2>
+    <img src="images/block4.png" alt="">
+
+  </div>
+  <div class="block5">
+    <div class="justext">
+
+
+      <h2>ABOUT OUR CONCEPT</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
+      <div class="littlebar">
+        <li><a href="#Pizza">Pizza</a></li>
+        <li><a href="#Modern_concept">Modern concept</a></li>
+        <li><a href="#Story_from_shef">Story from shef</a></li>
+        <li><a href="#Gallery">Gallery</a></li>
+      </div>
+    </div>
+  </div>
+  <div class="block6">
+
+    <div class="box1">
+      <h2>| THE RECIPE FOR THE MOST delicious pizza dough | <h2>
+      <h3>waiting for you!</h3>
+      <p>Price_15$<p>
+      <h4>12:00-15:00</h4>
+      <button>APPLY</button>
+    </div>
+    <h2>MASTERCLASS</h2>
+
+    <img src="images/block6.png" alt="">
+
+  </div>
+</body>
+
+</html>
